53d11abb655f42ef1567253db359faadb747733a
James Trachy <James.Trachy@concur.com>
2017-02-28 08:41:55 -0600

    Added ability to clear out test equities to the clear_test_data script

--- util/clear_test_data.py
+++ util/clear_test_data.py
@@ -18,11 +18,11 @@ for r in snapshot_records_to_delete:
     collection.delete_one({'id': r})
     print('record ' + r + ' deleted.')
 
+# Retrieve all the aggregates to clear out
 collection_a = db.aggregates
 aggregates = collection_a.find(filter={'ticker': 'JAMES'})
 aggregate_records_to_delete = []
 
-# Retrieve all the aggregates to clear out
 for a in aggregates:
     print('found aggregate with ticker = ' + a['ticker'] + '; id = ' + a['id'] + '...will delete')
     aggregate_records_to_delete.append(a['id'])
@@ -30,6 +30,21 @@ for a in aggregates:
 # Delete the snapshots that have been marked for deletion
 for r in aggregate_records_to_delete:
     collection_a.delete_one({'id': r})
+    print('record ' + r + ' deleted.')
+
+
+#Retrieve all the equities to clear out
+collection_e = db.equities
+equities = collection_e.find(filter={'ticker': 'JAMES'})
+equity_records_to_delete = []
+
+for e in equities:
+    print('found equity with ticker = ' + e['ticker'] + '; id = ' + e['id'] + '...will delete')
+    equity_records_to_delete.append(e['id'])
+
+# Delete the equities that have been marked for deletion
+for r in equity_records_to_delete:
+    collection_e.delete_one({'id': r})
     print('record ' + r + ' deleted.')
 
 client.close()
